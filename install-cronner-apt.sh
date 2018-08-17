dd4df7e572be7bddef4042d11f33c58de0600dd9
Travis Thieman <travis.thieman@gmail.com>
2018-08-17 14:26:25 -0400

    switch to curl

--- install-cronner-apt.sh
+++ install-cronner-apt.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-apt-get update && apt-get install -y iproute2 gzip tar wget
+apt-get update && apt-get install -y curl iproute2 gzip tar
 
-cd /tmp
-wget https://raw.githubusercontent.com/managedbyq/cronner-utils/master/install-cronner-common.sh
-chmod +x install-cronner-common.sh
-./install-cronner-common.sh
+curl -f https://raw.githubusercontent.com/managedbyq/cronner-utils/master/install-cronner-common.sh | sh
