f20b56aac76fe4c09f7f95831f637d2d42f5dd41
Travis Thieman <travis.thieman@gmail.com>
2018-08-17 14:49:51 -0400

    Follow redirects

--- install-cronner-apt.sh
+++ install-cronner-apt.sh
@@ -4,4 +4,4 @@ set -e
 
 apt-get update && apt-get install -y curl iproute2 gzip tar
 
-curl -f https://raw.githubusercontent.com/managedbyq/cronner-utils/master/install-cronner-common.sh | sh
+curl -L -f https://raw.githubusercontent.com/managedbyq/cronner-utils/master/install-cronner-common.sh | sh
--- install-cronner-common.sh
+++ install-cronner-common.sh
@@ -4,7 +4,7 @@ set -e
 
 cd /tmp
 
-curl -f -o cronner-linux-amd64-v1.0.0.tar.gz https://github.com/theckman/cronner/releases/download/v1.0.0/cronner-linux-amd64-v1.0.0.tar.gz
+curl -L -f -o cronner-linux-amd64-v1.0.0.tar.gz https://github.com/theckman/cronner/releases/download/v1.0.0/cronner-linux-amd64-v1.0.0.tar.gz
 export CHECKSUM=`openssl sha256 cronner-linux-amd64-v1.0.0.tar.gz | awk '{print $2}'`
 [ $CHECKSUM = b2e38b754d0c04c748e1548fa8d138616174dc67633a9848c7f422ddeb94cf14 ]
 gunzip cronner-linux-amd64-v1.0.0.tar.gz
@@ -12,6 +12,6 @@ tar -xvf cronner-linux-amd64-v1.0.0.tar
 mv cronner-linux-amd64-v1.0.0/cronner /usr/local/bin/cronner
 rm -rf /tmp/cronner*
 
-curl -o cronner-wrap https://raw.githubusercontent.com/managedbyq/cronner-utils/master/cronner-wrap
+curl -L -o cronner-wrap https://raw.githubusercontent.com/managedbyq/cronner-utils/master/cronner-wrap
 chmod +x cronner-wrap
-mv cronner-wrap /usr/local/bin/cronner-wrap
+mv cronner-wrap /usr/local/bin/cronner-
